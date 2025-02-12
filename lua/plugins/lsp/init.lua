9e7af2fb8b9c71dafbf75a2c27b3f4889484a4d0
Phillip Rearick <psrearick@gmail.com>
2025-02-12 14:25:43 -0500

    wip

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,31 +1,33 @@
 -- LSP configuration entry point
 
 return {
-  {
-    -- `lazydev` configures Lua LSP for Neovim configs, runtimes and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-      },
+    {
+        -- `lazydev` configures Lua LSP for Neovim configs, runtimes and plugins
+        -- used for completion, annotations and signatures of Neovim apis
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        opts = {
+            library = {
+                { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+            },
+        },
     },
-  },
-  {
-    -- Main LSP Configuration
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs and related tools to stdpath for Neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-      'j-hui/fidget.nvim',
-      'hrsh7th/cmp-nvim-lsp',
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+            'WhoIsSethDaniel/mason-tool-installer.nvim',
+            'j-hui/fidget.nvim',
+            'hrsh7th/cmp-nvim-lsp',
+        },
+        config = function()
+            -- Initialize Mason first
+            require('mason').setup()
+            -- Then set up the servers
+            require('plugins.lsp.servers').setup()
+            -- Finally set up completion
+            require('plugins.lsp.completion')
+        end,
     },
-    config = function()
-      require('plugins.lsp.servers')
-      require('plugins.lsp.completion')
-    end,
-  },
 }
