7adb7d0a48b50806deb0fc889c2a70a1b7df4407
Phillip Rearick <psrearick@gmail.com>
2025-02-13 09:41:30 -0500

    wip

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,15 +2,6 @@
 
 return {
   'akinsho/toggleterm.nvim',
-  dependencies = {
-      "folke/which-key.nvim",
-      optional = true,
-      opts = {
-        defaults = {
-          -- ["<leader>t"] = { name = "+terminal" },
-        },
-      },
-  },
   keys = { { "<leader>tt", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" } },
   version = "*",
   config = true
